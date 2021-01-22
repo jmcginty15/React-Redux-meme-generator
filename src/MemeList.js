e81e9369ee17daa550305cb7e0b9402c9dda257e
Jason McGinty <jason_mcginty@yahoo.com>
2021-01-22 16:20:41 -0600

    editing MemeList text

--- src/MemeList.js
+++ src/MemeList.js
@@ -4,7 +4,6 @@ const MemeList = ({ memes }) => {
     return (
         <div className="MemeList">
             <h1>Check out your memes</h1>
-            <h3>Or click a meme to remove it from the page</h3>
             {memes.map(meme => <Meme key={meme.id} meme={meme} />)}
         </div>
     )
